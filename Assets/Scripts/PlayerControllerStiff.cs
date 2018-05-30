1e6068f01889c66faa78ae001c0b01c53a2e1c5a
Jason Stallings <jason@stallin.gs>
2018-05-30 11:42:51 -0500

    Add readme.

---
+++ Assets/Scripts/PlayerControllerStiff.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerStiff : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 180f;
+    private Rigidbody2D player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalAxis = Mathf.Ceil(Input.GetAxis("Horizontal"));
+        var verticalAxis = Mathf.Ceil(Input.GetAxis("Vertical"));
+
+
+
+        Vector2 movement = Vector2.zero;
+
+        movement.x += (transform.right).x * horizontalAxis;
+        movement.y += (transform.up).y * verticalAxis;
+
+        movement.Normalize();
+
+        movement = movement * moveSpeed;
+        player.MovePosition((Vector2)(transform.position) + movement * Time.deltaTime);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# [o]
+
+## Reference
+
+### Space War for Atari 2600
+
+You can play it here: http://www.retrogames.cz/play_760-Atari2600.php?language=EN
+
+Use the game select menu to set it to game 14, then hit game reset.
+
+### Old version of this game
+
+[BJ](https://github.com/bjftw) and I created this a few years ago in Flash, but never finished it.
+
+https://www.youtube.com/watch?v=SPNzYSXy1Bg
