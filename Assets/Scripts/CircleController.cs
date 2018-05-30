b28b654125b9f9f882a3486574de3f7bebbe83b9
Jason Stallings <jason@stallin.gs>
2018-05-30 09:17:53 -0500

    Checkin files.

---
+++ Assets/Scripts/CircleController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 180f;
+    private Rigidbody2D circle;
+
+    void Start() {
+        circle = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //var horizontalAxis = Input.GetAxis("Horizontal");
+        //var verticalAxis = Input.GetAxis("Vertical");
+
+        // Turn left or right on left/right buttons
+        //transform.Rotate(0, 0, -horizontalAxis * Time.deltaTime * rotationSpeed);
+        //transform.Translate(new Vector2(1, -1) * 1 * Time.deltaTime * moveSpeed);
+
+        Vector2 movement = Vector2.zero;
+        // Turn left or right on left/right buttons
+        //transform.Rotate(0, 0, -horizontalAxis * Time.deltaTime * rotationSpeed);
+        //transform.right* Time.deltaTime* moveSpeed
+
+        // Move forward
+        //transform.Translate(Vector3.right * verticalAxis * Time.deltaTime * moveSpeed);
+
+        movement.x += (transform.right).x;
+        movement.y -= (transform.up).y;
+
+        movement.Normalize();
+
+        movement = movement * moveSpeed;
+        circle.MovePosition((Vector2)(transform.position) + movement * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerWrapping.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWrapping : MonoBehaviour {
+    Renderer[] renderers;
+    bool isWrappingX = false;
+    bool isWrappingY = false;
+
+    void Start()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+    }
+
+	private void Update()
+	{
+        ScreenWrap();
+	}
+
+	bool CheckRenderers()
+    {
+        foreach (var renderer in renderers)
+        {
+            // If at least one render is visible, return true
+            if (renderer.isVisible)
+            {
+                return true;
+            }
+        }
+
+        // Otherwise, the object is invisible
+        return false;
+    }
+
+    void ScreenWrap()
+    {
+        var isVisible = CheckRenderers();
+
+        if (isVisible)
+        {
+            isWrappingX = false;
+            isWrappingY = false;
+            return;
+        }
+
+        if (isWrappingX && isWrappingY)
+        {
+            return;
+        }
+
+        var cam = Camera.main;
+        var viewportPosition = cam.WorldToViewportPoint(transform.position);
+        var newPosition = transform.position;
+
+        if (!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -newPosition.x;
+
+            isWrappingX = true;
+        }
+
+        if (!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -newPosition.y;
+
+            isWrappingY = true;
+        }
+
+        transform.position = newPosition;
+    }
+}
