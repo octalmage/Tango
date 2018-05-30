419943fb32b8210c74ddea86cea9076e9d5f2c89
Jason Stallings <jason@stallin.gs>
2018-05-30 10:04:09 -0500

    Add collision detection.

--- Assets/Scripts/CircleController.cs
+++ Assets/Scripts/CircleController.cs
@@ -14,20 +14,7 @@ public class CircleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //var horizontalAxis = Input.GetAxis("Horizontal");
-        //var verticalAxis = Input.GetAxis("Vertical");
-
-        // Turn left or right on left/right buttons
-        //transform.Rotate(0, 0, -horizontalAxis * Time.deltaTime * rotationSpeed);
-        //transform.Translate(new Vector2(1, -1) * 1 * Time.deltaTime * moveSpeed);
-
         Vector2 movement = Vector2.zero;
-        // Turn left or right on left/right buttons
-        //transform.Rotate(0, 0, -horizontalAxis * Time.deltaTime * rotationSpeed);
-        //transform.right* Time.deltaTime* moveSpeed
-
-        // Move forward
-        //transform.Translate(Vector3.right * verticalAxis * Time.deltaTime * moveSpeed);
 
         movement.x += (transform.right).x;
         movement.y -= (transform.up).y;
@@ -37,4 +24,15 @@ public class CircleController : MonoBehaviour
         movement = movement * moveSpeed;
         circle.MovePosition((Vector2)(transform.position) + movement * Time.deltaTime);
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1f, 0.30196078f, 0.30196078f);
+       
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+	{
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
+	}
 }
