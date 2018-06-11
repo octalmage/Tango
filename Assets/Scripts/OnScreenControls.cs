91ec358e66d0b201eba45d6f1a0faab89876e13e
Jason Stallings <jason@stallin.gs>
2018-06-10 19:08:36 -0500

    Get rid of ship sound and fix fuel.

--- Assets/Scripts/OnScreenControls.cs
+++ Assets/Scripts/OnScreenControls.cs
@@ -13,8 +13,6 @@ public class OnScreenControls : MonoBehaviour, IPointerDownHandler, IPointerUpHa
         playerControllerScript = playerGameObject.GetComponent<PlayerControllerStiff>();
     }
 
-
-
     public void OnPointerDown(PointerEventData eventData)
 	{
         switch(direction) {
