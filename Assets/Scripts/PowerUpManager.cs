4f8a59c81a9f99bdbc2b0dbd05d6cc9e687d442b
Jason Stallings <jason@stallin.gs>
2018-06-15 21:03:22 -0500

    Powerups.

---
+++ Assets/Scripts/PowerUpManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpManager : MonoBehaviour
+{
+    public GameObject[] powerups;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(RandomWait());
+    }
+
+    IEnumerator RandomWait()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 2));
+            Vector2 randomPositionOnScreen = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+            GameObject powerup = Instantiate(powerups[0], randomPositionOnScreen, powerups[0].transform.rotation);
+            powerup.SetActive(true);
+        }
+    }
+}
